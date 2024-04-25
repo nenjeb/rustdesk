76d4a718162b5e5eeb375cd3780866c11b5f743d
nenjeb <83912665+nenjeb@users.noreply.github.com>
2024-04-25 13:41:49 +0200

    Update common.dart

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -1659,7 +1659,7 @@ Future<Size> _adjustRestoreMainWindowSize(double? width, double? height) async {
   const double maxHeight = 6480;
 
   final defaultWidth =
-      ((isDesktop || isWebDesktop) ? 1280 : kMobileDefaultDisplayWidth)
+      ((isDesktop || isWebDesktop) ? 500 : kMobileDefaultDisplayWidth)
           .toDouble();
   final defaultHeight =
       ((isDesktop || isWebDesktop) ? 720 : kMobileDefaultDisplayHeight)
